c5a976ab810b11646e541a04a5c4bd7161770514
ROHIT KUMAR <rohit9909mr@gmail.com>
2025-03-13 13:43:51 +0530

    init

---
+++ EmployeeManagement/Model/Entity/Employee.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Model.Entity
+{
+    public class Employee
+    {
+        
+    }
+}
